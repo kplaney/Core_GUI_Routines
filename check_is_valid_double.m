4993231330ad56355dce68660b53213229e9a7ba
Nick Hughes <nph123@gmail.com>
2012-01-15 15:11:06 -0800

    Moved utility files not only of use for GUIs to Core_Routines directory.

--- check_is_valid_double.m
+++
@@ -1,7 +0,0 @@
-
-function valid_double = check_is_valid_double(value)
-
-valid_double = true;
-valid_double = valid_double & ~isempty(value);
-valid_double = valid_double & ~isnan(value);
-valid_double = valid_double & (imag(value) == 0);
--- check_is_valid_integer.m
+++
@@ -1,8 +0,0 @@
-
-function valid_integer = check_is_valid_integer(value)
-
-valid_integer = true;
-valid_integer = valid_integer & ~isempty(value);
-valid_integer = valid_integer & ~isnan(value);
-valid_integer = valid_integer & (imag(value) == 0);
-valid_integer = valid_integer & (ceil(value) - value == 0);
